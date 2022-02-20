af7af2e9e5cf0538c4d5bbb14105df80fdc6164f
thiagodsd <thiagosd.2.18@gmail.com>
2022-02-20 12:15:44 -0300

    debug do readthedocs

--- src/taxi_em_nova_iorque/pipeline_registry.py
+++ src/taxi_em_nova_iorque/pipeline_registry.py
@@ -3,7 +3,8 @@ from typing import Dict
 
 from kedro.pipeline import Pipeline
 
-from taxi_em_nova_iorque.pipelines import transformacao, treino, producao
+from taxi_em_nova_iorque.pipelines import transformacao
+
 
 def register_pipelines() -> Dict[str, Pipeline]:
     """Register the project's pipelines.
@@ -13,10 +14,8 @@ def register_pipelines() -> Dict[str, Pipeline]:
     """
 
     pipeline_transformacao = transformacao.create_pipeline()
-    pipeline_treino = treino.create_pipeline()
 
     return {
-        "pipe_transformacao" : pipeline_transformacao,
-        "pipe_treino" : pipeline_treino,
-        "__default__": pipeline_transformacao + pipeline_treino
+        "pipe_transformacao": pipeline_transformacao,
+        "__default__": pipeline_transformacao,
     }
--- src/taxi_em_nova_iorque/pipelines/transformacao/pipeline.py
+++ src/taxi_em_nova_iorque/pipelines/transformacao/pipeline.py
@@ -6,22 +6,17 @@ generated using Kedro 0.17.6
 from kedro.pipeline import Pipeline, node
 from kedro.pipeline.modular_pipeline import pipeline
 
-from .nodes import (
-    limpeza, 
-    novas_features_simples, 
-    novas_features_complexas, 
-    concatena
-)
+from .nodes import limpeza, novas_features_simples, novas_features_complexas, concatena
 
 
 def create_pipeline(**kwargs) -> Pipeline:
     pipeline_limpeza = Pipeline(
         [
             node(
-                limpeza, 
-                inputs = "treino",
-                outputs = "tabela_limpa",
-                name = "node_treino_limpeza"
+                limpeza,
+                inputs="treino",
+                outputs="tabela_limpa",
+                name="node_treino_limpeza",
             )
         ]
     )
@@ -29,33 +24,37 @@ def create_pipeline(**kwargs) -> Pipeline:
     pipeline_novas_features = Pipeline(
         [
             node(
-                limpeza, 
-                inputs = "treino",
-                outputs = "tabela_limpa",
-                name = "node_treino_limpeza"
+                limpeza,
+                inputs="treino",
+                outputs="tabela_limpa",
+                name="node_treino_limpeza",
             ),
             node(
-                novas_features_simples, 
-                inputs = "tabela_limpa",
-                outputs = "tabela_feature_simples",
-                name = "node_treino_novas_features_simples"
+                novas_features_simples,
+                inputs="tabela_limpa",
+                outputs="tabela_feature_simples",
+                name="node_treino_novas_features_simples",
             ),
             node(
-                novas_features_complexas, 
-                inputs = ["tabela_limpa", "params:complex_treino", "params:complex_frac"],
-                outputs = dict(
-                    features_complexas = "tabela_feature_complexa",
-                    modelo_dbscan = "modelo_dbscan",
-                    modelo_kmeans = "modelo_kmeans"
+                novas_features_complexas,
+                inputs=["tabela_limpa", "params:complex_treino", "params:complex_frac"],
+                outputs=dict(
+                    features_complexas="tabela_feature_complexa",
+                    modelo_dbscan="modelo_dbscan",
+                    modelo_kmeans="modelo_kmeans",
                 ),
-                name = "node_treino_novas_features_complexas"
+                name="node_treino_novas_features_complexas",
             ),
             node(
-                concatena, 
-                inputs = ["tabela_limpa", "tabela_feature_simples", "tabela_feature_complexa"],
-                outputs = "treino_features",
-                name = "node_treino_concatena"
-            )
+                concatena,
+                inputs=[
+                    "tabela_limpa",
+                    "tabela_feature_simples",
+                    "tabela_feature_complexa",
+                ],
+                outputs="treino_features",
+                name="node_treino_concatena",
+            ),
         ]
     )
 
